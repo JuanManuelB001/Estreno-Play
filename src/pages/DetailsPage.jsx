818dd41d084210d9d2209542d3e5e83a8fef28e9
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2025-12-15 15:45:03 -0500

    Sistema de Estrellas

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -3,14 +3,15 @@ import { useParams } from "react-router-dom";
 import { getAdapter } from "axios";
 import { getApi } from "../data/httpClient";
 import { GetImages } from "../utils/GetImages";
-
+import { StartRate } from "../components/StartRate";
 export function DetailsPage(){
     let {movieId} = useParams();
     const [movies, setMovies] = useState([])
-
+    const [genero, setGenero]= useState([]);
     useEffect(()=>{
         getApi("/movie/"+movieId).then((data)=>{
             setMovies(data);
+            setGenero(data.genres);
         });
         
     },[movieId]);
@@ -19,7 +20,21 @@ export function DetailsPage(){
         <div>
             <div>
                 <img src={imagen} alt={movies.title}/>
+                <p>popularidad</p>
+                <StartRate popularity= {movies.popularity}/>
+                <p><strong>Popularity</strong> {movies.popularity}</p>
                 <p><strong>Tittle:</strong> {movies.title}</p>
+                <p><strong>Genero:</strong></p>
+                <ul>
+                    {genero?.map((gen)=>(
+                        <li key={gen.id}>
+                            {gen.name}
+                        </li>
+                    ))}
+                </ul>
+                <p>
+                    <strong>OverView</strong> {movies.overview}
+                </p>
             </div>
         </div>
     );
