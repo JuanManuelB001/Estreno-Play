664e3df3c58e69b2eda226827c59f24d48790a9a
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2026-03-02 12:58:14 -0500

    change vite.confing add base

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { LandingPage } from "../pages/LandingPage";
 import { DetailsPage } from "../pages/DetailsPage";
 import { ScrollToTop } from "../components/ScrollToTop";
